8e4b24770529678814c00861c8f182f93a88cedb
Jalal Albasri <jalal.albasri@gmail.com>
2017-12-12 02:40:12 +0300

    added poloniex

---
+++ Exchanges/poloniex.js
@@ -0,0 +1,36 @@
+const Exchange = require('./Exchange.js')
+const Poloniex = require('poloniex-api-node')
+const poloniex = new Poloniex(process.env.POLONIEX_API_KEY, process.env.POLONIEX_API_SECRET)
+
+// const prices = new Map()
+const poloniexExchange = new Exchange('poloniex')
+
+poloniex.on('message', (channelName, data, seq) => {
+    if (data.currencyPair.slice(0, 4) === 'USDT') {
+        let symbol = data.currencyPair.slice(5)
+        poloniexExchange.setPrice(sy)
+        prices.set(symbol, data.last)
+        console.log(`[POLONIEX] [${symbol}] $${data.last}`)
+    }
+})
+
+poloniex.on('open', () => {
+    console.log('Poloniex WebSocket connection open')
+    poloniex.subscribe('ticker')
+})
+
+poloniex.on('close', (reason, details) => {
+    console.log('Poloniex WebSocket connection disconnected ' + reason)
+})
+
+poloniex.on('error', (error) => {
+    console.log('An error has occured')
+})
+
+poloniex.openWebSocket({
+    version: 2
+})
+
+exports.getPrice = (symbol) => {
+    return prices.get(symbol)
+}
--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 require('dotenv').config()
 require('./Exchanges/bitfinex')
+require('./Exchanges/poloniex')
